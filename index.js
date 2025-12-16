f9e1d955f01289e88b9d7e598f457f0b22d60433
BaoAnh <baoanhtongthi@gmail.com>
2025-12-16 23:30:36 +0700

    Cập nhật lời chào mới

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+// const express = require('express');
+// const app = express();
+// app.get('/', (req, res) => res.send('API đang chạy!'));
+// app.listen(process.env.PORT || 3000);
+
 const express = require('express');
+const cors = require('cors'); // Thêm thư viện để Frontend truy cập được
 const app = express();
-app.get('/', (req, res) => res.send('API đang chạy!'));
-app.listen(process.env.PORT || 3000);
+
+// Sử dụng middleware cors
+app.use(cors()); 
+
+app.get('/', (req, res) => {
+    res.send('Kết nối Backend - Frontend thành công! Dữ liệu từ Render đã về BAO ANH.');
+});
+
+// Render sẽ tự cấp PORT thông qua biến môi trường
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
